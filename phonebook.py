53fced4fbb7a799dbc12f47b45bef8eaea1397ab
poralekhamhaher <hasanalmaher900@gmail.com>
2025-07-15 21:05:14 +0700

    Learned What Dictionary is For The First Time ;

---
+++ phonebook.py
@@ -0,0 +1,12 @@
+def get_contact_list():
+    names_and_numbers={"hasan":209320,
+                       "lawda":400934,
+                       "lassan":230923}
+    """so idk why but for some reason this structure within what i knew was a set {} is
+    called a "Dictionary" pardon if i spelled that wrong but damn . 
+            what it does is just the left side is called key and right is called value ; 
+            if i for some reason search with the key it will return the corresponding value
+            assigned with it .
+            
+            i have just made the dictionary inside a function so that if in the future ive to
+            import data from JSON or DB"""
