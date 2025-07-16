a8832a019ea0efab392386f679cd16475c2f09e1
poralekhamhaher <hasanalmaher900@gmail.com>
2025-07-16 12:22:58 +0700

    Project 1 : Question Added

---
+++ DAY2_16.7.2025/contact_sorter.py
@@ -0,0 +1,16 @@
+print("hi")
+
+"""💻 Project 1: contact_sorter.py
+
+📝 Description:
+You’ll take a dictionary of names → numbers, and print the sorted list alphabetically by name.
+
+☑️ Requirements:
+Define a function get_contacts() → returns a sample dictionary (5+ items)
+Define a function sort_contacts(contacts: dict) → returns sorted list of names
+Define a function display_sorted(contacts: dict) → prints each name + number in order
+Use if name == "main" to run your logic
+
+🧠 Bonus:
+Add option to sort by number instead of name
+Use input() to ask user how they want to sort"""
