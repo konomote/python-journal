18ccd00d76902a486cd64608fa9c03c125b50748
poralekhamhaher <hasanalmaher900@gmail.com>
2025-07-16 21:13:00 +0700

    First Working Run

--- DAY2_16.7.2025/contact_sorter.py
+++ DAY2_16.7.2025/contact_sorter.py
@@ -43,15 +43,27 @@ def get_contacts()->dict:
                        "developer":209320}
 
 def sort_contacts(contact_list:dict,sorted_by_number:bool,)->list:
-    if sorted_by_number:
+    if sorted_by_number==True:
+        return sorted(contact_list.items(), key=lambda item:item[1])
+    #so here in this line i had hundreds of questions so yeah
+        """first of all i asked does sorted() always return a list?
+        the answer was yes ; 
+        then i asked what does .items() return the key or value ;
+        so what happens after you use it with a dict ; it makes a tuple (imagine tuple as like
+        evolved knight from clash royale bro has armor on) of both the key and the value as a couple
+         ---
+          next i was like what is key=lambda ; so lambda makes a tiny no name function for one time
+          use and the item is the variable that we pass into that function ; 
+          and after we write color thats the end of that function and after the colon there is a
+          return which returns the 2nd item which is positioned in index 1 ; that is numbers
+          of the contact list here 
+            so just like x ; where i used item ; i can name the item anything ?
+            or it must be named item to cycle through the positions ; and also 
+            ne more thing ; what is key ; are there other things that i can replace \
+            lambda with by writing key=somethingElseIDK
+        YES . i can 100% rename the item to anything as i takes anything """
+    else:
         return sorted(contact_list.items())
-
-
-
-
-
-
-
 
 
 
@@ -72,6 +84,14 @@ def main():
 
     print (sorting_choice) #chekcing if user input (boolean) is correct or not
     ContactList=get_contacts()
+    print(sort_contacts(ContactList,sorting_choice))
+
+
+
+
+
+
+
 
 
 
