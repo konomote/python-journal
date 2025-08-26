42d69e8790291bdb541b5318fb178799a9e86d97
poralekhamhaher <hasanalmaher900@gmail.com>
2025-08-26 21:03:32 +0700

    Day 6 : Practicing Loops – Printing, Summing, and Iterating Through Lists

---
+++ DAY6_26.8.2025/basic_loops.py
@@ -0,0 +1,11 @@
+"""Day 6 (Aug 26): Loops Basics ✅ Today
+
+Topics: for, while, break, continue
+Practice Exercises:
+Print numbers 1 to 20 using a loop
+Sum numbers 1 to 100
+Print multiplication table for a number
+Loop through a list of strings and print each"""
+
+"=============================================================================="
+
