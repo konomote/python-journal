56cb7dc8604c930cfbf2322ac29458ce0e969480
poralekhamhaher <hasanalmaher900@gmail.com>
2025-08-28 10:02:54 +0700

    Day 7 : Calculator project (basic structure setup)

---
+++ DAY7_27.8.2025/calculator.py
@@ -0,0 +1,14 @@
+"""👉 Day 7: Build a CLI Calculator that supports +, -, *, /
+
+This means:
+User can run your Python file,
+Input two numbers and an operator (+ - * /),
+Program gives the result.
+Extra (optional if you want):
+Keep running until the user types exit.
+Handle invalid input gracefully."""
+
+"========================================================================="
+
+
+
