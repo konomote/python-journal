0a2edf0b3097300628ca1206296fd6f982c282e4
poralekhamhaher <hasanalmaher900@gmail.com>
2025-08-30 01:34:53 +0700

    Day 9 : Basic To-Do List Program With Storage ( OOP + JSON )

---
+++ DAY9-DAY10_29.8.2025/tasker.py
@@ -0,0 +1,19 @@
+"""
+👉 Task Manager (OOP + JSON)
+This project is a simple command-line Task Manager application.
+
+Features:
+- Add new tasks with a title and optional description
+- View all saved tasks
+- Remove tasks by their ID
+- Persistent storage using a JSON file (tasks are saved between runs)
+
+Learning Goals:
+- Practice Object-Oriented Programming (OOP) with Python classes
+- Understand how JSON works for structured data storage
+- Learn how to separate logic into multiple files (modular design)
+- Build a real-world CLI tool that goes beyond text file storage """
+
+
+"========================================================================="
+
